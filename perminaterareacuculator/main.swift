474246a37e862a0e6317abc896e96e7814a1fad7
yining wang <ywang@lcs.on.ca>
2021-01-29 12:18:40 -0500

    the input got finished

--- perminaterareacuculator/main.swift
+++ perminaterareacuculator/main.swift
@@ -20,31 +20,43 @@ import Foundation
 
 //MARK: Functions
 
-/// Finds the area of a rectangle
-/// - Parameter length: length of rectangle
-/// - Parameter width: width of rectangle
-/// - Returns: area of the rectangle
-func areaOfRectangle(length: Double, width: Double) -> Double {
-    
-    return length * width
-    
-}
+///// Finds the area of a rectangle
+///// - Parameter length: length of rectangle
+///// - Parameter width: width of rectangle
+///// - Returns: area of the rectangle
+//func areaOfRectangle(length: Double, width: Double) -> Double {
+//
+//    return length * width
+//
+//}
+//
+///// find the area of the circle
+///// - Parameter radius: the distance from the zenter of a circle to its outer edge
+///// - Returns: description
+//func areaOfCircle(radius: Double) -> Double {
+//    return Double.pi * pow(radius, 2.0)
+//}
 
-func areaOfCircle(radius: Double) -> Double {
-    return Double.pi * pow(radius, 2.0)
-}
+
 
 
 
 //MARK: Input
-let givenlength = 10.0
-let givenwidth = 2.0
+
+print("What Geometric Feature do you want to cauculate?")
+print("Cy: Cylender")
+print("Sp: Sphere")
+print("Co: Cone")
+print("Sp: Squared-based pyramid")
+print("Rp: Rectangualr prism")
+print("Sp: Squared-based pyramid")
+print("Tp: Triangular prism")//keep following output on the same line
+let type = readLine()!
 
 
 
 //MARK: Process
-let area = areaOfRectangle(length: givenlength, width: givenwidth)
 
 
 //MARK: Output
-print ("The area is \(area) square units")
+
